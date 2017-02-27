7ad8ac2a7d35638f63bf641e8ea88d0baf56773d
Jerrie Pelser <jerrie@jerriepelser.com>
2017-02-27 10:25:51 +0900

    Add WinForms and WPF Sample

---
+++ 00-Starter-Seed/WPF/WPFSample/MainWindow.xaml.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Auth0.OidcClient;
+using IdentityModel.OidcClient;
+
+namespace WPFSample
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        string[] _connectionNames = new string[]
+        {
+            "Username-Password-Authentication",
+            "google-oauth2",
+            "twitter",
+            "facebook",
+            "github",
+            "windowslive"
+        };
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private async void LoginButton_OnClick(object sender, RoutedEventArgs e)
+        {
+            string domain = ConfigurationManager.AppSettings["Auth0:Domain"];
+            string clientId = ConfigurationManager.AppSettings["Auth0:ClientId"];
+
+            var client = new Auth0Client(domain, clientId);
+
+            var extraParameters = new Dictionary<string, string>();
+
+            if (!string.IsNullOrEmpty(connectionNameComboBox.Text))
+                extraParameters.Add("connection", connectionNameComboBox.Text);
+
+            if (!string.IsNullOrEmpty(audienceTextBox.Text))
+                extraParameters.Add("audience", audienceTextBox.Text);
+
+            DisplayResult(await client.LoginAsync(extraParameters: extraParameters));
+        }
+
+        private void DisplayResult(LoginResult loginResult)
+        {
+            // Display error
+            if (loginResult.IsError)
+            {
+                resultTextBox.Text = loginResult.Error;
+                return;
+            }
+
+            // Display result
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendLine("Tokens");
+            sb.AppendLine("------");
+            sb.AppendLine($"id_token: {loginResult.IdentityToken}");
+            sb.AppendLine($"access_token: {loginResult.AccessToken}");
+            sb.AppendLine($"refresh_token: {loginResult.RefreshToken}");
+            sb.AppendLine();
+
+            sb.AppendLine("Claims");
+            sb.AppendLine("------");
+            foreach (var claim in loginResult.User.Claims)
+            {
+                sb.AppendLine($"{claim.Type}: {claim.Value}");
+            }
+
+            resultTextBox.Text = sb.ToString();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            connectionNameComboBox.ItemsSource = _connectionNames;
+        }
+    }
+}
---
+++ 00-Starter-Seed/WinForms/WindowsFormsSample/Form1.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Auth0.OidcClient;
+using IdentityModel.OidcClient;
+
+namespace WindowsFormsSample
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private async void loginButton_Click(object sender, EventArgs e)
+        {
+            string domain = ConfigurationManager.AppSettings["Auth0:Domain"];
+            string clientId = ConfigurationManager.AppSettings["Auth0:ClientId"];
+
+            var client = new Auth0Client(domain, clientId);
+
+            var extraParameters = new Dictionary<string, string>();
+
+            if (!string.IsNullOrEmpty(connectionNameComboBox.Text))
+                extraParameters.Add("connection", connectionNameComboBox.Text);
+
+            if (!string.IsNullOrEmpty(audienceTextBox.Text))
+                extraParameters.Add("audience", audienceTextBox.Text);
+
+            DisplayResult(await client.LoginAsync(extraParameters: extraParameters));
+        }
+
+        private void DisplayResult(LoginResult loginResult)
+        {
+            // Display error
+            if (loginResult.IsError)
+            {
+                resultTextBox.Text = loginResult.Error;
+                return;
+            }
+
+            // Display result
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendLine("Tokens");
+            sb.AppendLine("------");
+            sb.AppendLine($"id_token: {loginResult.IdentityToken}");
+            sb.AppendLine($"access_token: {loginResult.AccessToken}");
+            sb.AppendLine($"refresh_token: {loginResult.RefreshToken}");
+            sb.AppendLine();
+
+            sb.AppendLine("Claims");
+            sb.AppendLine("------");
+            foreach (var claim in loginResult.User.Claims)
+            {
+                sb.AppendLine($"{claim.Type}: {claim.Value}");
+            }
+
+            resultTextBox.Text = sb.ToString();
+        }
+    }
+}
